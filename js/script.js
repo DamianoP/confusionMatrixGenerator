9ed995cc90d85c60e182b233eebcf24e3189bfb6
DamianoP <damy06@hotmail.it>
2025-05-25 01:50:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let stats=document.getElementById("stats");
+let cfPrecision=2;
+let statsPrecision=4;
 function analyzeTable(){
     let absoluteCounter=0;
     let diagonalCounter=0;
@@ -28,7 +30,7 @@ function calculateCF(){
         makeCell("1-Precision","className",false)+
         makeCell("Recall","className",false)+
         makeCell("1-Recall","className",false)+
-        makeCell("f1-score","className",false)+
+        makeCell("F1 score","className",false)+
         "</tr>";
     let values=analyzeTable();
     let totalElement=values[0];
@@ -46,47 +48,69 @@ function calculateCF(){
             let idAbsolute="absolute"+i+j;
             let idPercentage="percentage"+i+j;
             let valueAbsolute=customParseInt(getValue(idAbsolute));
-            let percentage=twoDecimal(100*valueAbsolute/totalElement);
+            let percentage=trimDecimals(100*valueAbsolute/totalElement);
+            if(totalElement==0){
+                percentage=0;
+            }
             setHTML(idPercentage,percentage+"%<br><span>&nbsp;</span>");
             setValue("absolute"+i+numClasses,rowTotal);
         }
 
         // last column values
-        let precision=customParseInt(getValue("absolute"+i+i))/rowTotal;
-        let negative;
+        let precision=customParseInt(getValue("absolute"+i+i))/columnTotal;
+        let negative;     
+        let negativeBottom;   
+        if(isNaN(precision)){
+            precision=0;
+        }
         if(precision==0){
             negative=0;
-            if(rowTotal>0) negative=1;
+            if(columnTotal>0) negative=1;
         }
         else negative=1-precision;
+        negativeBottom=negative;
+        
         statsString+=makeCell(precision);
         statsString+=makeCell(negative);
-        let positive="<span style='color: green'>"+twoDecimal(100*precision)+"%</span>";
-        negative="<span style='color: red'>"+twoDecimal(100*negative)+"%</span>";
-        setHTML("percentage"+i+numClasses,positive+"<br>"+negative);
-        setHTML("label"+i,className);
 
         //last row values
-        let recall=customParseInt(getValue("absolute"+i+i))/columnTotal;
+        let recall=customParseInt(getValue("absolute"+i+i))/rowTotal;
+        negative=0;
+        if(isNaN(recall)){
+            recall=0;
+        }
         if(recall==0){
-            negative=0;
-            if(rowTotal>0) recall=1;
+            negative=1;
+            if(rowTotal>0) {recall=0;}
         }
         else negative=1-recall;
+
+        if (rowTotal==0){
+            negative=0;
+        }
+        let negativeRight="<span style='color: red'>"+trimDecimals(100*negative)+"%</span>";
+        let positive="<span style='color: green'>"+trimDecimals(100*recall)+"%</span>";
+        setHTML("percentage"+i+numClasses,positive+"<br>"+negativeRight);
+        setHTML("label"+i,className);
+
+
+
         statsString+=makeCell(recall);
         statsString+=makeCell(negative);
-        positive="<span style='color: green'>"+twoDecimal(100*recall)+"%</span>";
-        negative="<span style='color: red'>"+twoDecimal(100*negative)+"%</span>";
-        setHTML("sumColumnAbsolute"+i,"<input type='text' value='"+columnTotal+"'>");
+
+
+
+        positive="<span style='color: green'>"+trimDecimals(100*precision)+"%</span>";
+        negative="<span style='color: red'>"+trimDecimals(100*negativeBottom)+"%</span>";
+        setHTML("sumColumnAbsolute"+i,"<input disabled type='text' value='"+columnTotal+"'>");
         setHTML("sumColumnPercentage"+i,positive+"<br>"+negative);
         let f1Score=0;
-        try{
-            f1Score=2*(precision*recall)/(precision+recall);
-            macroF1+=f1Score;
-            weightedF1+=(f1Score*columnTotal);
-        }catch(e){
-            f1Score="nan";
+        if(precision+recall>0){
+            f1Score=2*(precision*recall)/(precision+recall);             
         }
+        macroF1+=f1Score;
+        weightedF1+=(f1Score*rowTotal);
+        console.log("weightedF1: "+f1Score+" * "+rowTotal+" = "+weightedF1);
         statsString+=makeCell(f1Score)+"<tr>";
     }
 
@@ -97,6 +121,18 @@ function calculateCF(){
         if(totalElement>0) negative=1;
     }
     else negative=1-accuracy;
+    
+    if(isNaN(accuracy)){
+        accuracy=0;
+    }    
+    if(isNaN(negative)){
+        negative=0;
+    }    
+    if(totalElement==0){
+        weightedF1=0;
+    }else{
+        weightedF1=weightedF1/totalElement;
+    }
     statsString+="<tr>" +
         makeCell("Accuracy","className",false)+
         makeCell(accuracy,"",true,5)+
@@ -111,13 +147,13 @@ function calculateCF(){
         "</tr>"+
         "<tr>"+
         makeCell("Weighted-F1","className",false)+
-        makeCell(weightedF1/totalElement,"",true,5)+
-        "</tr>";
+        makeCell(weightedF1,"",true,5)+
+    "</tr>";
 
 
-    let percentageFinal="<span style='color: green'>"+twoDecimal(100*accuracy)+"%</span>";
-    negative="<span style='color: red'>"+twoDecimal(100*negative)+"%</span>";
-    setHTML("absoluteFinal","<input type='text' value='"+diagonalValue+" / "+totalElement+"'>");
+    let percentageFinal="<span style='color: green'>"+trimDecimals(100*accuracy)+"%</span>";
+    negative="<span style='color: red'>"+trimDecimals(100*negative)+"%</span>";
+    setHTML("absoluteFinal","<input disabled type='text' value='"+diagonalValue+" / "+totalElement+"'>");
     setHTML("percentageFinal",percentageFinal+"<br>"+negative);
     //backupData();
     printStats(statsString);
@@ -128,15 +164,15 @@ function customParseInt(value){
     if(isNaN(x)) x=0;
     return x;
 }
-function twoDecimal(value){
-    return customParseInt(100*value)/100;
+function trimDecimals(value){
+    return value.toFixed(cfPrecision);
 }
 function downloadImage(i){
     $('input[type=text]').addClass('paddingTop');
     let CMname=document.getElementById("CMname").value.toLowerCase().replace(/\s/g, "");
     let id="#CM";
     if(i==1){
-        CMname+="table";
+        //CMname+="table";
     }else if(i==2){
         CMname+="stats";
         id="#stats";
@@ -196,18 +232,33 @@ function printStats(data){
     stats.innerHTML=data;
 }
 function customDecimal(value,numberOfDigit){
-    let x=10**numberOfDigit;
-    return customParseInt(x*value)/x;
+    //let x=10**numberOfDigit;
+    //return customParseInt(x*value)/x;
+    return value.toFixed(numberOfDigit);
 }
 function makeCell(value,className="",number=true,span=0){
-    if(number) value=customDecimal(value,4);
+    if(number) value=customDecimal(value,statsPrecision);
     return "<td colspan='"+span+"' class='"+className+"'>"+value+"</td>";
 }
 function makeString(name,value,number=true){
     if(number)
-        return name+": "+twoDecimal(value)+"<br>";
+        return name+": "+trimDecimals(value)+"<br>";
     else
         return name+": "+value+"<br>";
+}
+function changePrecision(){
+    let newValue=document.getElementById("CFprecision").value;
+    if(newValue){
+        cfPrecision=parseInt(newValue);
+        calculateCF();
+    }
+}
+function changeStatPrecision(){
+    let newValue=document.getElementById("statprecision").value;
+    if(newValue){
+        statsPrecision=parseInt(newValue);
+        calculateCF();
+    }
 }
 //reloadData();
 //autoStart();
